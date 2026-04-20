1f1b7b49b0cd3486c8531c24f9cce70a43cdded2
Daria babicheva <d.babichevva@yandex.ru>
2026-04-20 11:49:15 +0300

    Add new class - Automobile

---
+++ src/main/java/Automobile.java
@@ -0,0 +1,24 @@
+public class Automobile {
+    private String name;
+    private int speed;
+
+
+    public Automobile(String name, int speed) {
+        this.name = name;
+        this.speed = speed;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+
+    public int calculateDistance() {
+        return speed * 24;
+    }
+}
