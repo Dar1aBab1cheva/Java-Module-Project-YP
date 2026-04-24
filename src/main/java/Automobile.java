a1ec06c899338c219186a8f2e7d55d25cb0e29c7
Daria babicheva <d.babichevva@yandex.ru>
2026-04-24 20:11:34 +0300

    new update

--- src/main/java/Automobile.java
+++ src/main/java/Automobile.java
@@ -1,23 +1,19 @@
 public class Automobile {
-    private String name;
-    private int speed;
+    private final String name;
+    private final int speed;
 
-
+    // Конструктор
     public Automobile(String name, int speed) {
         this.name = name;
         this.speed = speed;
     }
 
-
+    // Геттеры
     public String getName() {
         return name;
     }
 
-    public int getSpeed() {
-        return speed;
-    }
-
-
+    // Метод для расчета дистанции за 24 часа
     public int calculateDistance() {
         return speed * 24;
     }
